1bb96958370fb5136da9d17c423c3e0a4e64bfc9
Colin Takeda <ctakeda@olin.edu>
2020-02-25 17:38:56 -0500

    Flow fields progress, taking inspiration off of example code and understanding underlying principles. Updating index for new script.

--- Flow Fields/flowfields.js
+++ Flow Fields/flowfields.js
@@ -1,39 +1,43 @@
-	float t = 0;
-	float i = 4;
-	float b = 20;
-
-	void setup() {
-	  size(600,600);
+	function setup() {
+		noLoop()
+	  	createCanvas(1000,1000)
+		draw()
 	}
 
-	void draw() {
-	  float max = TWO_PI;
-	  float min = -TWO_PI;
-	  background(245, 223, 152);
-	  stroke(109, 153, 122);
-	  fill(109, 153, 122);
-	  strokeWeight(2);
-	  for (float x = b; x <= width - b; x+=10) { 
-		for (float y = b; y <= height - b; y+=10) {
-		  float r = map(noise(x/200,y/200,t),0,1,min,max);
-		  float s = map(noise(x/200,y/200,t+200),0,1,3,10);
-		  drawCross(x,y,s,r);
+	function draw() {
+
+	  const max = TWO_PI
+	  const min = -TWO_PI
+	  var t = 0
+	  const i = 4
+	  const b = 20
+
+
+	  stroke(0,0,255)
+	  fill(109, 153, 122)
+	  strokeWeight(2)
+
+
+	  for (x = b; x <= width - b; x+=10) { 
+		for (y = b; y <= height - b; y+=10) {
+			const r = map(noise(x/200,y/200,t),0,1,min,max)
+			const s = map(noise(x/200,y/200,t+200),0,1,3,10)
+		 	 drawCross(x,y,s,r)
 		}
 	  }
-	  t += 0.001;
-	  //noLoop();
+		t+= 0.001
 	}
 
-	void drawCross(float x, float y, float radius, float start) {
-	  float x1 = x - (cos(start) * radius);
-	  float y1 = y - (sin(start) * radius);
-	  float x2 = x + (cos(start) * radius);
-	  float y2 = y + (sin(start) * radius);
-	  float x3 = x - (sin(start) * radius);
-	  float y3 = y + (cos(start) * radius);
-	  float x4 = x + (sin(start) * radius);
-	  float y4 = y - (cos(start) * radius);
+	function drawCross(x,y,radius,start) {
+		const x1 = x - (cos(start) * radius)
+	  	const y1 = y - (sin(start) * radius)
+	  	const x2 = x + (cos(start) * radius)
+	  	const y2 = y + (sin(start) * radius)
+	  	const x3 = x - (sin(start) * radius)
+	  	const y3 = y + (cos(start) * radius)
+	  	const x4 = x + (sin(start) * radius)
+	  	const y4 = y - (cos(start) * radius)
 	  
-	  line(x1,y1,x2,y2);
-	  line(x3,y3,x4,y4);
+	  line(x1,y1,x2,y2)
+	  line(x3,y3,x4,y4)
 	}
