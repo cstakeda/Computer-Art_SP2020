e81b8806938e9fd2ab352c456629cb2e1540f236
Colin Takeda <ctakeda@olin.edu>
2020-02-27 09:42:05 -0500

    Correctly labelled perlin noise work to correct initial folder name.

--- Perlin Noise/perlinnoise.js
+++ Perlin Noise/perlinnoise.js
@@ -1,7 +1,6 @@
-
-	
 	function setup() {
 		createCanvas(1000,1000)
+		background(254)
 	}
 
 	function mousePressed(){ // Attempt to get canvas to reload. Believed to be isse=ue with noise()
@@ -13,14 +12,14 @@
 	function draw() {
 
 		noLoop()
-		//frameRate(1)
 
 		const max = TWO_PI
 	  	const min = -TWO_PI
 	  	const borderoffset  = 25 //Border from edge
 	  	const c = 1000 //Tuning parameter
 	  	const spacing = 6.1 //Spacing inbetween crosses
-	  	var t = 0 
+		  var t = 0 
+		  
 	  	stroke(5)
 	  	strokeWeight(1.9)
 
