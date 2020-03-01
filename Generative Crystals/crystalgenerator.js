e0b2225089c0b15d9c71c2bbb91312be06cfb7d3
Colin Takeda <ctakeda@olin.edu>
2020-03-01 16:49:27 -0500

    Added CenterShape,RingOfShapes, and SteppedHexagons

--- Generative Crystals/crystalgenerator.js
+++ Generative Crystals/crystalgenerator.js
@@ -3,6 +3,8 @@ const HEX_SIZE = 500
 const SIDES = 6
 let PALETTE = []
 
+let layers = []
+
 function setup() {
 
   createCanvas(750,750,SVG)
@@ -23,17 +25,54 @@ function setup() {
   // ]
 }
 
-function draw() { // Main function call
+function draw() { // Main function call for p5.js
 
   background(230)
 
-  const circles = new Circles()
-  circles.render()
+  // const circles = new Circles()
+  // circles.render()
   
-  const simpleLines = new SimpleLines()
-  simpleLines.render()
+  // const simpleLines = new SimpleLines()
+  // simpleLines.render()
 
-  const outlineShape = new OutlineShape()
-  outlineShape.render()
+  // const outlineShape = new OutlineShape()
+  // outlineShape.render()
 
+  // const centeredShape = new CenteredShape()
+  // centeredShape.render()
+
+  // const ringofShapes = new RingOfShapes()
+  // ringofShapes.render()
+
+  // const steppedHexagons = new SteppedHexagons()
+  // steppedHexagons.render()
+
+  let picker = random(1)
+  if (picker > .3){
+    layers.push(new Circles())
+  }
+  picker = random(1)
+  if (picker > .3){
+    layers.push(new SimpleLines())
+  }
+  picker = random(1)
+  if (picker > .3){
+    layers.push(new OutlineShape())
+  }
+  picker = random(1)
+  if (picker > .3){
+    layers.push(new CenteredShape())
+  }
+  picker = random(1)
+  if (picker > .3){
+    layers.push(new RingOfShapes())
+  }
+  picker = random(1)
+  if (picker > .3){
+    layers.push(new SteppedHexagons())
+  }
+  
+  layers.forEach(layer =>{
+    layer.render()
+  })
 }
